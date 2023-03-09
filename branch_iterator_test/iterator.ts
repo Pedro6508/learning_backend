a102f2f8f93e62692c80bd36cdcf77608dbe2c05
Pedro Vinicius Coelho belem <pevine6508@gmail.com>
2023-03-09 10:01:08 -0300

    Fix: iterador refeito apartir do teste.

--- branch_iterator_test/iterator.ts
+++ branch_iterator_test/iterator.ts
@@ -47,96 +47,35 @@ export function riverRun(map: RiverMap): RiverMap {
   return map;
 }
 
-class WaterMove implements Iterator<[number, number] | undefined> {
-  private position: [number, number];
-  private value: Floor;
-  readonly map: RiverMap;
-  readonly limits: MapLimits;
+export class IterMove implements IterableIterator<[number, number]> {
+  readonly moveList: Array<[number, number]> = [
+    [1, 0],
+    [0, 1],
+    [-1, 0],
+    [0, -1],
+  ];
+  private moveIndex = 0;
 
-  private done: boolean;
-  private moveIndex: number;
-  readonly moveList: Array<[number, number]>;
+  constructor() {}
 
-  private safeMove(next: [number, number]): boolean {
-    let result = false;
-    const nextPosition = [
-      this.position[0] + next[0],
-      this.position[1] + next[1],
-    ];
-
-    if (
-      nextPosition[0] >= this.limits.hight.beg &&
-      nextPosition[0] <= this.limits.hight.end &&
-      nextPosition[1] >= this.limits.length.beg &&
-      nextPosition[1] <= this.limits.length.end
-    ) {
-      result = true;
-    }
-
-    return result;
-  }
-
-  constructor(
-    position: [number, number],
-    value: Floor,
-    map: RiverMap,
-    limits: MapLimits,
-  ) {
-    this.position = position;
-    if (value === Floor.Water) {
-      this.value = value;
-    } else {
-      throw new Error("WaterMove Iterator works only in Floor.Water");
-    }
-    this.map = map;
-    this.limits = limits;
-
-    this.done = false;
-    this.moveIndex = 0;
-    this.moveList = [
-      [1, 0],
-      [0, 1],
-      [-1, 0],
-      [0, -1],
-    ];
-  }
-
-  ignoreAndNext(): IteratorResult<[number, number] | undefined> {
-    if (this.moveIndex >= this.moveList.length - 1) {
+  public next(): IteratorResult<[number, number]> {
+    if (this.moveIndex >= this.moveList.length) {
       return {
         done: true,
-        value: undefined,
+        value: this.moveList[this.moveList.length - 1],
       };
     } else {
+      const result: IteratorResult<[number, number]> = {
+        done: false,
+        value: this.moveList[this.moveIndex],
+      };
+
       this.moveIndex += 1;
-      return this.next();
+      return result;
     }
   }
 
-  public next(): IteratorResult<[number, number] | undefined> {
-    if (this.done == true) {
-      return {
-        done: true,
-        value: undefined,
-      };
-    } else {
-      const [i, j] = this.moveList[this.moveIndex];
-
-      if (this.safeMove([i, j]) === true) {
-        switch (this.map.data[i][j]) {
-          case Floor.Flat: {
-            return {
-              done: false,
-              value: [i, j],
-            };
-          }
-          default: {
-            return this.ignoreAndNext();
-          }
-        }
-      } else {
-        return this.ignoreAndNext();
-      }
-    }
+  [Symbol.iterator](): IterableIterator<[number, number]> {
+    return this;
   }
 }
